5bd8969cbe747643d9750820b7e404af150f1b9f
ddojai <favianred@gmail.com>
2020-12-19 17:41:54 +0900

    스프링부트 시큐리티
    - 3강 시큐리티 회원가입
    - https://www.youtube.com/watch?v=aOYqLaQLb4o&list=PL93mKxaRDidERCyMaobSLkvSPzYtIk0Ah&index=3

---
+++ securit-basic/src/main/java/io/github/ddojai/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package io.github.ddojai.repository;
+
+import io.github.ddojai.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
