986e84545f983d23e124c485625dafbd32f5d5e5
ddojai <favianred@gmail.com>
2020-12-19 17:49:54 +0900

    스프링부트 시큐리티
    - 4강 시큐리티 로그인
    - https://www.youtube.com/watch?v=riwrVN1JVbU&list=PL93mKxaRDidERCyMaobSLkvSPzYtIk0Ah&index=4

--- securit-basic/src/main/java/io/github/ddojai/repository/UserRepository.java
+++ securit-basic/src/main/java/io/github/ddojai/repository/UserRepository.java
@@ -4,4 +4,6 @@ import io.github.ddojai.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  public User findByUsername(String username);
 }
