079e736c32d78ff9e8d6cd6b0a0d97e709de5ffe
ddojai <favianred@gmail.com>
2020-12-23 20:47:48 +0900

    스프링부트 시큐리티
    - 6강 구글 로그인 준비
    - https://www.youtube.com/watch?v=9ui2i-SgBpk&list=PL93mKxaRDidERCyMaobSLkvSPzYtIk0Ah&index=6

--- securit-basic/src/main/java/io/github/ddojai/Application.java
+++ securit-basic/src/main/java/io/github/ddojai/Application.java
@@ -1,12 +1,17 @@
 package io.github.ddojai;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
+  private static final String APPLICATION_LOCATIONS = "spring.config.location="
+    + "classpath:application.yml,"
+    + "classpath:application-oauth.yml";
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    new SpringApplicationBuilder(Application.class)
+      .properties(APPLICATION_LOCATIONS)
+      .run(args);
   }
 }
