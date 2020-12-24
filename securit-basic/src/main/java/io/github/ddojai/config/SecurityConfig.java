f67f506e027ead1c920e4dcb23e2ca7b4b8263d5
ddojai <favianred@gmail.com>
2020-12-24 15:21:21 +0900

    스프링부트 시큐리티
    - 7강 구글 회원 프로필 정보 받아오기
    - https://www.youtube.com/watch?v=nVCEJv6eA-w&list=PL93mKxaRDidERCyMaobSLkvSPzYtIk0Ah&index=7

--- securit-basic/src/main/java/io/github/ddojai/config/SecurityConfig.java
+++ securit-basic/src/main/java/io/github/ddojai/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.ddojai.config;
 
+import io.github.ddojai.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,11 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+// oauth2
+// 1.코드받기(인증), 2.엑세스토큰(권한)
+// 3.사용자프로필 정보를 가져오고
+// 4-1.그 정보를 토대로 회원가입을 자동으로 진행
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록 됨
 // secured 어노테이션 활성화, preAuthorize & postAuthorize 어노테이션 활성화
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private PrincipalOauth2UserService principalOauth2UserService;
 
   @Bean
   public BCryptPasswordEncoder encodePwd() {
@@ -34,6 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .defaultSuccessUrl("/")
       .and()
       .oauth2Login()
-      .loginPage("/loginForm");
+      .loginPage("/loginForm")
+      .userInfoEndpoint()
+      .userService(principalOauth2UserService); // 구글 로그인 완료된 뒤의 후처리가 필요함. Tip. 코드X, (엑세스토큰 + 사용자프로필정보 O)
   }
 }
