1c79229e35629fcb4a68557f6460268c9222b2f6
ddojai <favianred@gmail.com>
2020-12-30 22:47:16 +0900

    스프링부트 시큐리티
    - 19강 - jwt를 위한 yml파일 세팅
    - https://www.youtube.com/watch?v=IRh6Eh3pWEY&list=PL93mKxaRDidERCyMaobSLkvSPzYtIk0Ah&index=19

---
+++ security-jwt/src/main/java/io/github/ddojai/Application.java
@@ -0,0 +1,16 @@
+package io.github.ddojai;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+  private static final String APPLICATION_LOCATIONS = "spring.config.location="
+    + "classpath:application.yml";
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(Application.class)
+      .properties(APPLICATION_LOCATIONS)
+      .run(args);
+  }
+}
