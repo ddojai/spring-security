4f5e1f8adacfd5b30b727c0dc4c59983cfea50b8
ddojai <favianred@gmail.com>
2020-12-31 18:24:10 +0900

    스프링부트 시큐리티 23강
    - jwt 임시 토큰 만들어서 테스트 해보기
    - https://www.youtube.com/watch?v=IRh6Eh3pWEY&list=PL93mKxaRDidERCyMaobSLkvSPzYtIk0Ah&index=23

--- security-jwt/src/main/java/io/github/ddojai/filter/MyFilter3.java
+++ security-jwt/src/main/java/io/github/ddojai/filter/MyFilter3.java
@@ -1,14 +1,34 @@
 package io.github.ddojai.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
     throws IOException, ServletException {
-    System.out.println("필터3");
-    chain.doFilter(request, response);
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+
+    // id, pw를 통해 로그인 완료시 토큰을 만들고 응답
+    // 그 다음부터 요청시 헤더에 토큰값 전달
+    // 토큰이 서버에서 만든 토큰이 맞는지만 검증하면 됨
+    if (req.getMethod().equals("POST")) {
+      System.out.println("포스트 요청됨");
+      String headerAuth = req.getHeader("Authorization");
+      System.out.println(headerAuth);
+      System.out.println("필터3");
+
+      if (headerAuth.equals("cos")) {
+        chain.doFilter(req, res);
+      } else {
+        PrintWriter out = res.getWriter();
+        out.println("인증안됨");
+      }
+    }
   }
 }
